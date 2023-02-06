17060235881dd656c03bf30d9465e60d69027c0b
Chikeziem Nwadinobi <khaled305.on@gmail.com>
2023-02-06 14:04:46 +0100

    added date to firebase store and updated env file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9_kSW_TFgxBOYcE0jyDLiOqcjxrFZ1Tc",
-  authDomain: "foodapp-327d5.firebaseapp.com",
-  databaseURL: "https://foodapp-327d5-default-rtdb.firebaseio.com",
-  projectId: "foodapp-327d5",
-  storageBucket: "foodapp-327d5.appspot.com",
-  messagingSenderId: "9717855678",
-  appId: "1:9717855678:web:5288993000c8ff48ab75ef"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
